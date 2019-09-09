6dda8a31d937401d62a658aafac727da6b2b7a99
LeAnhToan <leanhtoan@vietbank.com.vn>
2019-09-09 11:42:00 +0700

    Add new feature

--- VietbankWebsite/VietbankWebsite/Repository/AboutVietbankRepository.cs
+++ VietbankWebsite/VietbankWebsite/Repository/AboutVietbankRepository.cs
@@ -33,21 +33,21 @@ namespace VietbankWebsite.Repository
         public async Task<ListNews> GetListNews(int idCate, string cateName, string aliasCate, string lang, int page, int pageSize)
         {
             var lstNewsItems = await (from a in _context.VbPosts
-                                 join b in _context.VbPostCategories on a.Id equals b.post_ID
-                                 join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
-                                 where b.category_ID.Equals(idCate)
-                                 where c.language.Equals(lang)
-                                 where a.post_status.Equals(4)
-                                 orderby a.Id descending
-                                 select new ListNewsItem()
-                                 {
-                                     CategoryName = cateName,
-                                     Title = c.post_title,
-                                     Description = c.post_excerpt,
-                                     Url = $"{aliasCate}/{c.post_url}",
-                                     Thumbnail = a.post_thumbnail,
-                                     PublishDate = a.post_date
-                                 }).Skip(3).Skip(pageSize * page).Take(pageSize).ToListAsync();
+                                      join b in _context.VbPostCategories on a.Id equals b.post_ID
+                                      join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
+                                      where b.category_ID.Equals(idCate)
+                                      where c.language.Equals(lang)
+                                      where a.post_status.Equals(4)
+                                      orderby a.Id descending
+                                      select new ListNewsItem()
+                                      {
+                                          CategoryName = cateName,
+                                          Title = c.post_title,
+                                          Description = c.post_excerpt,
+                                          Url = $"{aliasCate}/{c.post_url}",
+                                          Thumbnail = a.post_thumbnail,
+                                          PublishDate = a.post_date
+                                      }).Skip(3).Skip(pageSize * page).Take(pageSize).ToListAsync();
 
             var totalSize = from a in _context.VbPosts
                             join b in _context.VbPostCategories on a.Id equals b.post_ID
@@ -59,54 +59,54 @@ namespace VietbankWebsite.Repository
             return new ListNews()
             {
                 ListNewsItems = lstNewsItems,
-                PageSize = ComputePagination(await totalSize.CountAsync(),pageSize)
+                PageSize = ComputePagination(await totalSize.CountAsync(), pageSize)
             };
         }
 
         public async Task<IntroduceDetail> IntroduceDetail(int postId, string lang)
         {
             var introduceDetail = await (from a in _context.VbPostTranslates
-                                         join b in _context.VbPosts  on a.post_ID equals b.Id
-                                   where a.post_ID.Equals(postId)
-                                   where a.language.Equals(lang)
-                                   select new IntroduceDetail()
-                                   {
-                                       Title = a.post_title,
-                                       Content = a.post_content,
-                                       Image = b.ping_status ?? "/img/banner_page/vietbank-bg.jpg"
-                                   }).FirstOrDefaultAsync();
+                                         join b in _context.VbPosts on a.post_ID equals b.Id
+                                         where a.post_ID.Equals(postId)
+                                         where a.language.Equals(lang)
+                                         select new IntroduceDetail()
+                                         {
+                                             Title = a.post_title,
+                                             Content = a.post_content,
+                                             Image = b.ping_status ?? "/img/banner_page/vietbank-bg.jpg"
+                                         }).FirstOrDefaultAsync();
             return introduceDetail;
         }
 
         public async Task<IntroduceVietbank> IntroduceVietbank(string alias, string lang)
         {
             var introduce = await (from a in _context.VbCategories
-                             join b in _context.VbPostCategories on a.ID equals b.category_ID
-                             join d in _context.VbCategoryTranslates on a.ID equals d.category_ID
-                             join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
-                             where a.slug.Equals(alias)
-                             where c.language.Equals(lang)
-                             where d.language.Equals(lang)
-                             select new IntroduceVietbank()
-                             {
-                                 Title = d.name,
-                                 Image = a.thumbnail?? "/img/banner_page/vietbank-bg.jpg",
-                                 Content = c.post_content
-                             }).FirstOrDefaultAsync();
-            return introduce;           
+                                   join b in _context.VbPostCategories on a.ID equals b.category_ID
+                                   join d in _context.VbCategoryTranslates on a.ID equals d.category_ID
+                                   join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
+                                   where a.slug.Equals(alias)
+                                   where c.language.Equals(lang)
+                                   where d.language.Equals(lang)
+                                   select new IntroduceVietbank()
+                                   {
+                                       Title = d.name,
+                                       Image = a.thumbnail ?? "/img/banner_page/vietbank-bg.jpg",
+                                       Content = c.post_content
+                                   }).FirstOrDefaultAsync();
+            return introduce;
         }
 
         public async Task<LeadershipDetail> LeadershipDetail(string alias, string lang)
         {
             var postID = await _context.VbPostTranslates.Where(x => x.post_url.Equals(alias)).SingleAsync();
             var leadershipDetail = await (from a in _context.VbPostTranslates
-                                    where a.post_url.Equals(alias)
-                                    where a.language.Equals(lang)
-                                    select new LeadershipDetail()
-                                    {
-                                        Title = a.post_title,
-                                        Content = a.post_content
-                                    }).FirstOrDefaultAsync();
+                                          where a.post_url.Equals(alias)
+                                          where a.language.Equals(lang)
+                                          select new LeadershipDetail()
+                                          {
+                                              Title = a.post_title,
+                                              Content = a.post_content
+                                          }).FirstOrDefaultAsync();
             if (leadershipDetail == null)
             {
                 var langTemp = lang == "vi" ? "en" : "vi";
@@ -122,7 +122,7 @@ namespace VietbankWebsite.Repository
             return leadershipDetail;
         }
 
-        public async Task<IEnumerable<TopThreeNewsToCate>> TopThreeNewsToCate(int idCate, string aliasCate,string lang)
+        public async Task<IEnumerable<TopThreeNewsToCate>> TopThreeNewsToCate(int idCate, string aliasCate, string lang)
         {
             int[] arrayCate = null;
             if (idCate == 0)
@@ -144,22 +144,22 @@ namespace VietbankWebsite.Repository
                               }).Take(3).ToListAsync();
             }
             return await (from a in _context.VbPosts
-                               join b in _context.VbPostCategories on a.Id equals b.post_ID
-                               join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
-                               where b.category_ID.Equals(idCate)
-                               where c.language.Equals(lang)
-                               orderby a.Id descending
-                               select new TopThreeNewsToCate()
-                               {
-                                   Title = c.post_title,
-                                   Description = c.post_excerpt,
-                                   Thumbnail = a.post_thumbnail,
-                                   Url = $"{aliasCate}/{c.post_url}",
-                                   PublishDate = a.post_date
-                               }).Take(3).ToListAsync();
+                          join b in _context.VbPostCategories on a.Id equals b.post_ID
+                          join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
+                          where b.category_ID.Equals(idCate)
+                          where c.language.Equals(lang)
+                          orderby a.Id descending
+                          select new TopThreeNewsToCate()
+                          {
+                              Title = c.post_title,
+                              Description = c.post_excerpt,
+                              Thumbnail = a.post_thumbnail,
+                              Url = $"{aliasCate}/{c.post_url}",
+                              PublishDate = a.post_date
+                          }).Take(3).ToListAsync();
         }
 
-        public async Task<NewsDetail> GetNewsDetail(string alias,string lang)
+        public async Task<NewsDetail> GetNewsDetail(string alias, string lang)
         {
             var news = from a in _context.VbPostTranslates
                        join b in _context.VbPosts on a.post_ID equals b.Id
@@ -176,38 +176,31 @@ namespace VietbankWebsite.Repository
 
         public async Task<IEnumerable<RandomNewsVietbank>> GetRamdomNewsToCategory(int idCate, string aliasCate, string lang)
         {
-            try
+            var r = new Random();
+            var lstPost = await _context.VbPostCategories.Where(x => x.category_ID.Equals(idCate)).Select(x => x.post_ID).ToArrayAsync();
+            int[] result = new int[8];
+            for (int i = 0; i < 8; i++)
             {
-                var r = new Random();
-                var lstPost = await _context.VbPostCategories.Where(x => x.category_ID.Equals(idCate)).Select(x => x.post_ID).ToArrayAsync();
-                int[] result = new int[8];
-                for (int i = 0; i < 8; i++)
-                {
-                    var elementRand = r.Next(0, lstPost.Count() - 1);
-                    result[i] = lstPost.ElementAt(elementRand);
-                }
+                var elementRand = r.Next(0, lstPost.Count() - 1);
+                result[i] = lstPost.ElementAt(elementRand);
+            }
 
-                var listRandom = await (from a in _context.VbPosts
-                                        join b in _context.VbPostCategories on a.Id equals b.post_ID
-                                        join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
-                                        where b.category_ID.Equals(idCate)
-                                        where c.language.Equals(lang)
-                                        where result.Distinct().Contains(b.post_ID)
-                                        where a.post_status.Equals(4)
-                                        orderby a.Id descending
-                                        select new RandomNewsVietbank()
-                                        {
-                                            Title = c.post_title,
-                                            Url = $"{aliasCate}/{c.post_url}",
-                                            PublishedDate = a.post_date.ToString("dd/MM/yyyy")
-                                        }).Take(5).ToListAsync();
-                return listRandom;
-            }
-            catch (Exception ex)
-            {
-                return new List<RandomNewsVietbank>();
-            }
-            
+            var listRandom = await (from a in _context.VbPosts
+                                    join b in _context.VbPostCategories on a.Id equals b.post_ID
+                                    join c in _context.VbPostTranslates on b.post_ID equals c.post_ID
+                                    where b.category_ID.Equals(idCate)
+                                    where c.language.Equals(lang)
+                                    where result.Distinct().Contains(b.post_ID)
+                                    where a.post_status.Equals(4)
+                                    orderby a.Id descending
+                                    select new RandomNewsVietbank()
+                                    {
+                                        Title = c.post_title,
+                                        Url = $"{aliasCate}/{c.post_url}",
+                                        PublishedDate = a.post_date.ToString("dd/MM/yyyy")
+                                    }).Take(5).ToListAsync();
+            return listRandom;
+
         }
 
         private async Task<int[]> ListPostRandomNewsToCategory(int idCate, VietbankContext vietbankContext)
@@ -233,13 +226,13 @@ namespace VietbankWebsite.Repository
 
     public interface IAboutVietbankRepository
     {
-        Task<IntroduceVietbank> IntroduceVietbank(string alias,string lang);
-        Task<IntroduceDetail> IntroduceDetail(int postId,string lang);
-        Task<LeadershipDetail> LeadershipDetail(string alias,string lang);
-        Task<IEnumerable<TopThreeNewsToCate>> TopThreeNewsToCate(int idCate,string aliasCate,string lang);
-        Task<ListNews> GetListNews(int idCate,string cateName, string aliasCate, string lang,int page,int pageSize);
+        Task<IntroduceVietbank> IntroduceVietbank(string alias, string lang);
+        Task<IntroduceDetail> IntroduceDetail(int postId, string lang);
+        Task<LeadershipDetail> LeadershipDetail(string alias, string lang);
+        Task<IEnumerable<TopThreeNewsToCate>> TopThreeNewsToCate(int idCate, string aliasCate, string lang);
+        Task<ListNews> GetListNews(int idCate, string cateName, string aliasCate, string lang, int page, int pageSize);
 
-        Task<NewsDetail> GetNewsDetail(string alias,string lang);
+        Task<NewsDetail> GetNewsDetail(string alias, string lang);
         Task<IEnumerable<RandomNewsVietbank>> GetRamdomNewsToCategory(int idCate, string aliasCate, string lang);
         Task<BankCodeDataTable> GetVbBankCodes();
     }
--- VietbankWebsite/VietbankWebsite/bin/Release/netcoreapp3.0/publish/wwwroot/js/site.js
+++ VietbankWebsite/VietbankWebsite/bin/Release/netcoreapp3.0/publish/wwwroot/js/site.js
@@ -1,3 +1,40 @@
 ﻿(function () {
-    
+    initialSearchInforChange();    
 })();
+var delay = (function () {
+    var timer = 0;
+    return function (callback, ms) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
+function initialSearchInforChange() {
+    $('#search-infor').keyup(function () {
+        if ($(this).val().length > 3) {
+            $(this).parent().find('i').removeClass('fa fa-search').addClass('fa fa-close');
+            $(this).parent().find('i').click(function () {
+                $('#search-results').html(null);
+                $(this).removeClass('fa fa-close').addClass('fa fa-search');
+                $('#search-infor').val(null);
+            });
+            
+            delay(function () {
+                $.get('/api/home/search?key=' + $('#search-infor').val(), function (data) {
+                    //console.log(data);
+                    generateResultSearch(data)
+                });
+            }, 300);
+        } else {
+            $(this).parent().find('i').removeClass('fa fa-close').addClass('fa fa-search');
+            $('#search-results').html(null);
+        }
+    });
+}
+
+function generateResultSearch(result) {
+    var listResult = '<i class="list-group-item"><strong>' + result.length + ' </strong>kết quả tìm kiếm cho <b>“' + $('#search-infor').val() + '”</b></i>';
+    for (var i = 0; i < result.length; i++) {
+        listResult += '<a class="list-group-item" href="' + result[i].url + '"> <i class="zmdi zmdi-file-text"></i><span>' + result[i].title+'</span></a>';
+    }
+    $('#search-results').html(listResult);
+}
