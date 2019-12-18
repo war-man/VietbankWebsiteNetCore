e845384eee65ab21a935d1cea71f57f37e7fc19e
LeAnhToan <leanhtoan@vietbank.com.vn>
2019-12-18 09:08:17 +0700

    Initial module tra cuu hoa don

---
+++ VietbankWebsite/VietbankWebsite/Modules/Modular.Modules.TraCuuHoaDon/Controllers/TraCuuHoaDonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modular.Modules.TraCuuHoaDon.Controllers
+{
+    [Route("tracuuhoadon")]
+    public class TraCuuHoaDonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VietbankWebsite/VietbankWebsite/Modules/Modular.Modules.TraCuuHoaDon/ModuleInitializer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Modular.Modules.TraCuuHoaDon
+{
+    public class ModuleInitializer
+    {
+    }
+}
